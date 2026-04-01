e57e80fe284475f9af63c6c2e39c60aa5c994729
BFreestone <bradenfreestone@hotmail.com>
2026-04-01 21:26:32 +0800

    added character save slots

--- Assets/Saves/IOJson.cs
+++ Assets/Saves/IOJson.cs
@@ -16,31 +16,88 @@ public class IOJson : MonoBehaviour
         string dataToSave = JsonUtility.ToJson(data);
         File.WriteAllText(path, dataToSave);
     }
-    public void SavePlayer()
+
+    public void SavePlayer(string path)
     {
         GetDataToSave();
-        SaveJsonFile(filePath, saveLoadPlayerData);
+        SaveJsonFile(path, saveLoadPlayerData);
     }
 
-    private void Start()
+    PlayerData LoadData(string path)
     {
-        //SavePlayer();
-        LoadPlayer();
-    }
-
-    PlayerData LoadData()
-    {
-        string loadedData = File.ReadAllText(filePath);
+        string loadedData = File.ReadAllText(path);
         return JsonUtility.FromJson<PlayerData>(loadedData);
     }
+
     void SendLoadedDataToPlayer()
     {
-        GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHandler>().playerData = saveLoadPlayerData;
+        PlayerHandler handler = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHandler>();
+        handler.playerData = saveLoadPlayerData;
+        handler.ApplyPlayerData();
     }
 
-    public void LoadPlayer()
+    public void LoadPlayer(string path)
     {
-        saveLoadPlayerData = LoadData();//debug
+        saveLoadPlayerData = LoadData(path);
         SendLoadedDataToPlayer();
     }
+
+    string GetSlotPath(int slot)
+    {
+        return $"{Application.dataPath}/PlayerData{slot}.json";
+    }
+    public void Save1()
+    {
+        SavePlayer(GetSlotPath(1));
+    }
+
+    public void Save2()
+    {
+        SavePlayer(GetSlotPath(2));
+    }
+
+    public void Save3()
+    {
+        SavePlayer(GetSlotPath(3));
+    }
+
+    public void Load1()
+    {
+        LoadPlayer(GetSlotPath(1));
+    }
+
+    public void Load2()
+    {
+        LoadPlayer(GetSlotPath(2));
+    }
+
+    public void Load3()
+    {
+        LoadPlayer(GetSlotPath(3));
+    }
+
+    //public void SavePlayer()
+    //{
+    //    GetDataToSave();
+    //    SaveJsonFile(filePath, saveLoadPlayerData);
+    //}
+
+    //PlayerData LoadData()
+    //{
+    //    string loadedData = File.ReadAllText(filePath);
+    //    return JsonUtility.FromJson<PlayerData>(loadedData);
+    //}
+
+    //void SendLoadedDataToPlayer()
+    //{
+    //    GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHandler>().playerData = saveLoadPlayerData;
+
+    //}
+    
+
+    //public void LoadPlayer()
+    //{
+    //    saveLoadPlayerData = LoadData();//debug
+    //    SendLoadedDataToPlayer();
+    //}
 }
--- Assets/Saves/PlayerHandler.cs
+++ Assets/Saves/PlayerHandler.cs
@@ -9,17 +9,37 @@ public class PlayerHandler : MonoBehaviour
     public CharacterController characterController;
     //public Inventory inventory;
 
+
     private void Start()
+    {
+        ApplyPlayerData();
+    }
+    public void ApplyPlayerData()
     {
         //turn the character controller off
         characterController.enabled = false;
-        //name the player
-        this.gameObject.name = playerData.characterName;
         //move the character position
-        this.transform.position = playerData.playerPosition;
+        transform.position = playerData.playerPosition;
         //move the character rotation
-        this.transform.rotation = Quaternion.Euler(playerData.playerRotation);
+        transform.rotation = Quaternion.Euler(playerData.playerRotation);
+        //name the player
+        gameObject.name = playerData.characterName;
         //turn on the character controller
         characterController.enabled = true;
     }
+
+
+    //public void ApplyPlayerData()
+    //{
+    //    //turn the character controller off
+    //    characterController.enabled = false;
+    //    //name the player
+    //    this.gameObject.name = playerData.characterName;
+    //    //move the character position
+    //    this.transform.position = playerData.playerPosition;
+    //    //move the character rotation
+    //    this.transform.rotation = Quaternion.Euler(playerData.playerRotation);
+    //    //turn on the character controller
+    //    characterController.enabled = true;
+    //}
 }
